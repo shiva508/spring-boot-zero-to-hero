4f910cab8d94d129f71c64ae4ea26de06a6df570
shiva508 <dasarishiva1@gmail.com>
2021-10-03 10:43:37 +0530

    joinpoint

--- src/main/java/com/pool/config/aop/aopuser/before/pointcut/AopUserPointCutDeclaration.java
+++ src/main/java/com/pool/config/aop/aopuser/before/pointcut/AopUserPointCutDeclaration.java
@@ -3,23 +3,16 @@ package com.pool.config.aop.aopuser.before.pointcut;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class AopUserPointCutDeclaration {
-	@Pointcut("execution(* com.pool.service.aop.*.*(..))")
-	public void aopUserPointCut() {
-		System.out.println("POINT CUT EXPRESSION");
-	}
-	
-	@Before("aopUserPointCut()")
-	public void beforePointcutExpression() {
-		System.out.println("BEFORE POINTCUT CALLED");
-	}
-	
-	@Before("aopUserPointCut()")
-	public void beforePointcutExpressionToSaveToSolar() {
-		System.out.println("BEFORE POINTCUT CALLED-->SAVE TO SOLAR");
+
+	@Before("com.pool.config.aop.aopuser.expression.AopUserAspectCommonExpressions.aopAllowPackageNoSetterPointCut()")
+	public void logToSolarAsync() {
+		System.out.println("LOG TO CLOUD ASYNC");
 	}
 }
